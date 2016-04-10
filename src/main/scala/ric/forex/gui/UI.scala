7a53d519b6254a519e1fb91b5d60dcc162560fad
Ric Holland <ric.holland89@gmail.com>
2016-04-10 11:23:11 +0100

    Changes to UI
    Changed the title of the UI box and replaced the setCurr button with a conversion direction ComboBox. This box allows the conversion to change from converting to GBP to from GBP. Evaluation is also changed to reflect the new funcionality.

--- src/main/scala/ric/forex/gui/UI.scala
+++ src/main/scala/ric/forex/gui/UI.scala
@@ -6,14 +6,14 @@ import ric.forex.backend.Rates
 
 class UI extends MainFrame {
   val rates = Rates("http://api.fixer.io/latest?base=GBP")
-  title = "GUI Program #1"
+  title = "GBP Currency Conversion"
   preferredSize = new Dimension(1300, 700)
   val currSelect = new ComboBox[String](rates.rates.keySet.toList.sorted)
-  val setCurr = new Button("Set Currency")
+  val convDir = new ComboBox[String](List("From GBP", "To GBP"))
   val currLine = new BoxPanel(Orientation.Horizontal) {
-    contents += currSelect
+    contents += convDir
     contents += Swing.HStrut(10)
-    contents += setCurr
+    contents += currSelect
   }
 
   val amount = new TextField
@@ -38,17 +38,25 @@ class UI extends MainFrame {
     border = Swing.EmptyBorder(20, 20, 20, 20)
   }
 
-  listenTo(setCurr)
   listenTo(setAmount)
 
   reactions += {
-    case ButtonClicked(`setCurr`) => eval()
     case ButtonClicked(`setAmount`) => eval()
   }
 
   def eval(){
+    val fromGBP = convDir.selection.item match {
+      case "From GBP" => true
+      case "To GBP" => false
+      case _ => throw new Exception("Incorrect conversion direction option.")
+    }
     val rateCode = currSelect.selection.item
-    val num = (amount.text.toDouble) * (rates.getRate(rateCode).getOrElse(0.0))
-    results.text = num.toString + " " + rateCode
+    if (fromGBP) {
+      val num = (amount.text.toDouble) * (rates.getRate(rateCode).getOrElse(0.0))
+      results.text = num.toString + " " + rateCode
+    } else {
+      val num2 = (amount.text.toDouble) / (rates.getRate(rateCode).get)
+      results.text = num2.toString + " GBP"
+    }
   }
 }
